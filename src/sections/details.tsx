d124904f10bf05647934c9a17c6f0e1e0c62eafe
Brad Thompson <bthompson6225@gmail.com>
2023-01-12 15:43:42 -0700

    Finalize UI design

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -4,11 +4,11 @@ const Details = () => {
 	const colSpan = useBreakpointValue({ base: 2, md: 1 });
 	return (
 		<VStack w='full' h='full' p={10} spacing={10} alignItems='flex-start'>
-			<VStack spacing='3' alignItems='flex-start'>
-				<Heading size='2xl'>Your Details</Heading>
-				<Text>If you already have an account, click here to log in</Text>
+			<VStack spacing={3} alignItems='flex-start'>
+				<Heading size='2xl'>Your details</Heading>
+				<Text>If you already have an account, click here to log in.</Text>
 			</VStack>
-			<SimpleGrid columns={2} columnGap={3} rowGap='full'>
+			<SimpleGrid columns={2} columnGap={3} rowGap={6} w='full'>
 				<GridItem colSpan={colSpan}>
 					<FormControl>
 						<FormLabel>First Name</FormLabel>
@@ -21,7 +21,7 @@ const Details = () => {
 						<Input placeholder='Doe' />
 					</FormControl>
 				</GridItem>
-				<GridItem colSpan={colSpan}>
+				<GridItem colSpan={2}>
 					<FormControl>
 						<FormLabel>Address</FormLabel>
 						<Input placeholder='Blvd. Broken Dreams 21' />
@@ -45,10 +45,10 @@ const Details = () => {
 					</FormControl>
 				</GridItem>
 				<GridItem colSpan={2}>
-					<Checkbox defaultChecked>Ship to billing address</Checkbox>
+					<Checkbox defaultChecked>Ship to billing address.</Checkbox>
 				</GridItem>
 				<GridItem colSpan={2}>
-					<Button colorScheme='brand' size='lg' w='full'>
+					<Button variant='primary' size='lg' w='full'>
 						Place Order
 					</Button>
 				</GridItem>
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,42 @@
-import { extendTheme, theme as base, withDefaultColorScheme, withDefaultVariant } from "@chakra-ui/react";
+import {
+  extendTheme,
+  theme as base,
+  withDefaultColorScheme,
+  withDefaultVariant,
+} from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-        50:  '#f5fee5',
+const inputSelectStyles = {
+  variants: {
+    filled: {
+      field: {
+        _focus: {
+          borderColor: 'brand.500',
+        },
+      },
+    },
+  },
+  sizes: {
+    md: {
+      field: {
+        borderRadius: 'none',
+      },
+    },
+  },
+};
+
+const brandRing = {
+  _focus: {
+    ring: 2,
+    ringColor: 'brand.500',
+  },
+};
+
+const theme = extendTheme(
+  {
+    colors: {
+      brand: {
+        50: '#f5fee5',
         100: '#e1fbb2',
         200: '#cdf781',
         300: '#b8ee56',
@@ -18,6 +51,36 @@ const theme = extendTheme({
     fonts: {
       heading: `Montserrat, ${base.fonts?.heading}`,
       body: `Inter, ${base.fonts?.body}`,
+    },
+    components: {
+      Button: {
+        variants: {
+          primary: (props: Record<string, any>) => ({
+            rounded: 'none',
+            ...brandRing,
+            color: mode('white', 'gray.800')(props),
+            backgroundColor: mode('brand.500', 'brand.200')(props),
+
+            _hover: {
+              backgroundColor: mode('brand.600', 'brand.300')(props),
+            },
+
+            _active: {
+              backgroundColor: mode('brand.700', 'brand.400')(props),
+            },
+          }),
+        },
+      },
+      Input: { ...inputSelectStyles },
+      Select: { ...inputSelectStyles },
+      Checkbox: {
+        baseStyle: {
+          control: {
+            borderRadius: 'none',
+            ...brandRing,
+          },
+        },
+      },
     },
   },
   withDefaultColorScheme({
